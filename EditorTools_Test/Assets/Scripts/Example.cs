c5fe02335c088ae90da3026645bc34e1f314301d
Pim <pimhoreman@gmail.com>
2021-03-18 16:07:39 +0100

    Deleted some useless files and make the project ready

--- EditorTools_Test/Assets/Scripts/Example.cs
+++ EditorTools_Test/Assets/Scripts/Example.cs
@@ -1,25 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Reflection;
+﻿using UnityEngine;
 
+/// <summary>
+/// Example Class To Fill Folder
+/// </summary>
 public class Example : MonoBehaviour
 {
-    private ReflectionExample reflectionExample;
-    private BindingFlags bindingFlag;
     // Start is called before the first frame update
     void Start()
     {
-        MethodInfo[] methods = typeof(ReflectionExample).GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
-        foreach (MethodInfo method in methods)
-        {
-            Debug.Log(method);
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+       
     }
 }
