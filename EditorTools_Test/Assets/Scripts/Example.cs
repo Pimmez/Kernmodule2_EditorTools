0538dc5a3c122d4e227a72c1ba4af2920075acd8
Pim <pimhoreman@gmail.com>
2021-03-18 15:51:40 +0100

    Added The .gitignore and Added Startup unity files

---
+++ EditorTools_Test/Assets/Scripts/Example.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+
+public class Example : MonoBehaviour
+{
+    private ReflectionExample reflectionExample;
+    private BindingFlags bindingFlag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MethodInfo[] methods = typeof(ReflectionExample).GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+        foreach (MethodInfo method in methods)
+        {
+            Debug.Log(method);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
