2e72672ea39ca98e7beee7717a8ea48e15de3fcc
Pim <pimhoreman@gmail.com>
2021-03-18 16:47:03 +0100

    Made/Tested some editorwindow scripts for information

---
+++ EditorTools_Test/Assets/CustomEditorWindow/Editor/CustomEditorTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CustomEditorTest : EditorWindow
+{
+    private string objectEditorName;
+    private int objectID = 1;
+    private GameObject objectToSpawn;
+    private float objectScale;
+    private Vector3 objectVector;
+    private float radius = 5f;
+
+    [MenuItem("Tools/Custom Editor")]
+    public static void ShowWindow()
+    {
+        GetWindow(typeof(CustomEditorTest));
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Test Unity Editor", EditorStyles.boldLabel);
+
+        objectEditorName = EditorGUILayout.TextField("Object Editor Name", objectEditorName);
+        objectID = EditorGUILayout.IntField("Object ID", objectID);
+        objectScale = EditorGUILayout.Slider("Scale", objectScale, 0.5f, 3f);
+        objectVector = EditorGUILayout.Vector3Field("Object XYZ", objectVector);
+        radius = EditorGUILayout.FloatField("radius", radius);
+        objectToSpawn = EditorGUILayout.ObjectField("Object", objectToSpawn, typeof(GameObject), false) as GameObject;
+    }
+}
---
+++ EditorTools_Test/Assets/CustomEditorWindow/Editor/MenuTest.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MenuTest : EditorWindow
+{
+    [MenuItem("Tools/Do Something")]
+    static void DoSomething()
+    {
+        Debug.Log("I did something");
+    }
+}
