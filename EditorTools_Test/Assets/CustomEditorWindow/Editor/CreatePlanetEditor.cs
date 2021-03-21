1e69a6a3dbac9c6d728a21f3f96a10853fe5c062
Pim <pimhoreman@gmail.com>
2021-03-21 15:53:44 +0100

    Can Create Full Mesh
    -Mesh Creation Complete with 6 subdivisions
    -Added Mesh Save in the editor
    -Trying to save the gameobject as a whole (WIP)
    -Added Earth normal and diffuse map.
    -Mostly Code Changes

--- EditorTools_Test/Assets/CustomEditorWindow/Editor/CreatePlanetEditor.cs
+++ EditorTools_Test/Assets/CustomEditorWindow/Editor/CreatePlanetEditor.cs
@@ -5,12 +5,16 @@ using UnityEditor;
 
 public class CreatePlanetEditor : EditorWindow
 {
+    [HideInInspector]
+    private GameObject planet;
+    
     private int planetID;
     private string planetName;
-    private float planetRadius = 1f;
-    private int planetSubdivisions = 0;
-    private Material material;
 
+    [Range(0, 6)]
+    private int planetSubdivisions = 0;
+    private float planetRadius = 1f;
+    private Material material;
 
     /// <summary>
     /// Create menu item and show an editorwindow.
@@ -30,10 +34,20 @@ public class CreatePlanetEditor : EditorWindow
         planetRadius = EditorGUILayout.FloatField("Planet Radius: ", planetRadius);
         planetSubdivisions = EditorGUILayout.IntField("Planet Subdivisions: ", planetSubdivisions);
         material = EditorGUILayout.ObjectField("Standard Material: ", material, typeof(Material), false) as Material;
-      
+
+        if (GUILayout.Button("Save Mesh"))
+        {
+            OnMeshSaved();
+        }     
+
         if (GUILayout.Button("Create Planet"))
         {
             CreatePlanet();
+        }
+
+        if (GUILayout.Button("Save Planet"))
+        {
+            OnPlanetSaved(planet);
         }
     }
 
@@ -51,106 +65,49 @@ public class CreatePlanetEditor : EditorWindow
             return;
         }
 
-        GameObject _planet = new GameObject();
+        planet = new GameObject();
 
-        _planet.name = planetName + planetID;
+        planet.name = planetName + planetID;
 
-        _planet.AddComponent<MeshFilter>();
-        _planet.AddComponent<MeshRenderer>();
-        _planet.GetComponent<MeshRenderer>().material = material;
-        _planet.GetComponent<MeshFilter>().mesh = CreateMesh(planetSubdivisions, planetRadius);
+        planet.AddComponent<MeshFilter>();
+        planet.AddComponent<MeshRenderer>();
+        planet.AddComponent<MeshCollider>();
+        planet.GetComponent<MeshRenderer>().material = material;
+        planet.GetComponent<MeshFilter>().mesh = OctahedronSphereCreator.Create(planetSubdivisions, planetRadius);
 
         planetID++;
     }
 
-    public static Mesh CreateMesh(int subdivisions, float radius)
+    private void OnMeshSaved()
     {
-        if (subdivisions < 0)
+        string _path = EditorUtility.SaveFilePanelInProject(
+            "Save Custom Mesh",
+            "Custom Mesh",
+            "asset",
+            "Specify where to save the mesh.");
+        if (_path.Length > 0)
         {
-            subdivisions = 0;
-            Debug.LogWarning("Octahedron Sphere subdivisions increased to minimum, which is 0.");
+            Mesh _mesh = OctahedronSphereCreator.Create(planetSubdivisions, planetSubdivisions);
+            MeshUtility.Optimize(_mesh);
+            AssetDatabase.CreateAsset(_mesh, _path);
+            AssetDatabase.SaveAssets();
         }
-        else if (subdivisions > 6)
-        {
-            subdivisions = 6;
-            Debug.LogWarning("Octahedron Sphere subdivisions decreased to maximum, which is 6.");
-        }
-
-        Vector3[] vertices = {
-            Vector3.down, Vector3.down, Vector3.down, Vector3.down,
-            Vector3.forward,
-            Vector3.left,
-            Vector3.back,
-            Vector3.right,
-            Vector3.up, Vector3.up, Vector3.up, Vector3.up
-        };
-
-        int[] triangles = {
-            0, 4, 5,
-            1, 5, 6,
-            2, 6, 7,
-            3, 7, 4,
-
-             8, 5, 4,
-             9, 6, 5,
-            10, 7, 6,
-            11, 4, 7
-        };
-
-        Vector3[] normals = new Vector3[vertices.Length];
-        Normalize(vertices, normals);
-
-        Vector2[] uv = new Vector2[vertices.Length];
-        CreateUV(vertices, uv);
-
-        if (radius != 1f)
-        {
-            for (int i = 0; i < vertices.Length; i++)
-            {
-                vertices[i] *= radius;
-            }
-        }
-
-        Mesh mesh = new Mesh();
-        mesh.name = "Octahedron Sphere";
-        mesh.vertices = vertices;
-        mesh.normals = normals;
-        mesh.uv = uv;
-        mesh.triangles = triangles;
-        return mesh;
     }
 
-    private static void CreateUV(Vector3[] vertices, Vector2[] uv)
+    private void OnPlanetSaved(GameObject gameObject)
     {
-        float previousX = 1f;
-        for (int i = 0; i < vertices.Length; i++)
+        string _path = EditorUtility.SaveFilePanelInProject(
+            "Save planetary gameobject",
+            gameObject.name,
+            "prefab",
+            "Specify where to save the planet.");
+        if (_path.Length > 0)
         {
-            Vector3 v = vertices[i];
-            if (v.x == previousX)
-            {
-                uv[i - 1].x = 1f;
-            }
-            previousX = v.x;
-            Vector2 textureCoordinates;
-            textureCoordinates.x = Mathf.Atan2(v.x, v.z) / (-2f * Mathf.PI);
-            if (textureCoordinates.x < 0f)
-            {
-                textureCoordinates.x += 1f;
-            }
-            textureCoordinates.y = Mathf.Asin(v.y) / Mathf.PI + 0.5f;
-            uv[i] = textureCoordinates;
-        }
-        uv[vertices.Length - 4].x = uv[0].x = 0.125f;
-        uv[vertices.Length - 3].x = uv[1].x = 0.375f;
-        uv[vertices.Length - 2].x = uv[2].x = 0.625f;
-        uv[vertices.Length - 1].x = uv[3].x = 0.875f;
-    }
-
-    private static void Normalize(Vector3[] vertices, Vector3[] normals)
-    {
-        for (int i = 0; i < vertices.Length; i++)
-        {
-            normals[i] = vertices[i] = vertices[i].normalized;
+            Mesh _mesh;
+            _mesh = Resources.Load<Mesh>("Assets/Resources/");
+            gameObject.GetComponent<MeshFilter>().mesh = _mesh;
+            PrefabUtility.SaveAsPrefabAsset(gameObject, _path);
+            Selection.activeObject = gameObject;
         }
     }
 }
